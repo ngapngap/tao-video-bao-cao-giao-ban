cc91ca545bb86b4d2943a08885eeee8e3346d9da
Dinh Mai Nam <namdm.danang@vss.local>
2026-05-07 09:56:55 +0700

    chore: bump version to 0.4.3

--- app/version.py
+++ app/version.py
@@ -1,5 +1,5 @@
 """Thông tin phiên bản và endpoint cập nhật của ứng dụng."""
 
-__version__ = "0.4.2"
+__version__ = "0.4.3"
 VERSION_URL = "https://api.github.com/repos/ngapngap/tao-video-bao-cao-giao-ban/releases/latest"
 DOWNLOAD_URL_TEMPLATE = "https://github.com/ngapngap/tao-video-bao-cao-giao-ban/releases/download/{tag}/{filename}"
